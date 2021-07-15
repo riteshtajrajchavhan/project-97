3e2f21e2d1ee6a183d04968813f790b8f4716964
riteshtajrajchavhan <74711100+riteshtajrajchavhan@users.noreply.github.com>
2021-07-15 10:43:15 +0530

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,10 @@
+  print("Guess a number (between 1 and 9")
+
+guess=int(input("Enter your guess "))
+
+if guess === 9:
+    print("Congratulations You Won !!!")
+else:   
+    print("your guess was too low")
+
+  
